10f814d247212fab9a39b8727d00e6cd5b0794bb
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-03-05 00:16:46 +0530

    Alpha (#8)
    * cooment remove
    
    * created Dashboard
    
    * Created the Dashboard
    
    Created Dashboard and files for tabs
    
    * Alpha (#5) (#7)
    
    * Alpha (#5)
    
    * cooment remove
    
    * created Dashboard
    
    * Created the Dashboard
    
    Created Dashboard and files for tabs
    
    * react package change
    
      "react-scripts": "5.0.1",
            "react-scripts": "^5.0.1",
    
    * Change to start
    
    ---------
    
    Co-authored-by: Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gayathri Gamage <122828456+gayathrip2000@users.noreply.github.com>

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,7 +9,16 @@ function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+
+            <a className="dropdown-item" href="/home">Home</a>
+                {/* {user.isUser ? :<a className="navbar-brand a1" href="/dashboard">Dashboard</a>} */}
+
                 {user.isUser ? <a className="dropdown-item" href="/home">Home</a> :<a className="navbar-brand a1" href="/dashboard">Dashboard</a>}
+
+
+                {user.isUser ? <a className="dropdown-item" href="/home">Home</a> :<a className="navbar-brand a1" href="/dashboard">Dashboard</a>}
+
                 <button
                     class="navbar-toggler"
                     type="button"
