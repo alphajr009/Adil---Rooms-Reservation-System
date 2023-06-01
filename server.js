4ef22e04772dfa7c0619fa248fc99999d810cfa6
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-01 16:53:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const path = require("path");
 
-
-const app =express();
+const app = express();
 
 const dbconfig = require('./db')
 const roomsRoute = require('./routes/roomsRoute')
@@ -19,9 +19,15 @@ app.use('/api/sellers',sellerRoute )
 
 const port = process.env.PORT || 5000;
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set the static folder
+  app.use(express.static("client/build"));
 
-if(process.env.NODE_ENV == 'production'){
-    app.use(express.static('client/build'))
+  // Serve the index.html file for all non-API routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => console.log('Node Server Started using Nodemon!'));
