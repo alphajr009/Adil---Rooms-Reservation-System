bcc7846da4700f93ad6e4aa80014c6a9f4c5a11a
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-01 13:50:49 +0530

    blog update

--- client/src/Dashboard/Blog.js
+++ client/src/Dashboard/Blog.js
@@ -3,10 +3,10 @@ import React from 'react'
 function Blog() {
   return (
 
-    <div><h1>Blog testing 2 test again in gaya </h1></div>
-=======
-
-    <div><h1>Blog testing 2 test again in gaya </h1></div>
+    <div>
+      <div><h1>Blog testing 2 test again in gaya </h1></div>
+      <div><h1>Blog testing 2 test again in gaya </h1></div>
+    </div>
 
 
   )
