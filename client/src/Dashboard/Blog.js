9816f4ed500eca2df9e9b612ae8a60c84d39fd4e
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-06-01 14:04:06 +0530

    blog update (#17)
    * cooment remove
    
    * created Dashboard
    
    * Created the Dashboard
    
    Created Dashboard and files for tabs
    
    * Alpha (#5) (#7)
    
    * Alpha (#5)
    
    * cooment remove
    
    * created Dashboard
    
    * Created the Dashboard
    
    Created Dashboard and files for tabs
    
    * react package change
    
      "react-scripts": "5.0.1",
            "react-scripts": "^5.0.1",
    
    * Change to start
    
    ---------
    
    Co-authored-by: Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
    
    * tesing git
    
    * delete extra
    
    * test again
    
    * heroku  update push
    
    heroku  update push
    
    * blog update
    
    * package.json
    
    ---------
    
    Co-authored-by: Gayathri Gamage <122828456+gayathrip2000@users.noreply.github.com>

--- client/src/Dashboard/Blog.js
+++ client/src/Dashboard/Blog.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Blog() {
   return (
 
-    <div><h1>Blog testing 2 test again in gaya </h1></div>
+    <div>
+      <div><h1>Blog testing 2 test again in gaya </h1></div>
+      <div><h1>Blog testing 2 test again in gaya </h1></div>
+    </div>
+
 
 
     <div><h1>Blog testing 2 test again in gaya </h1></div>
