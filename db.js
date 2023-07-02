dbb304714c03fb7bca50e67bb08e2d491fb23bd6
Pasindu Randika <99603230+alphajr009@users.noreply.github.com>
2023-07-02 13:39:14 +0530

    Commit Adil

--- db.js
+++ db.js
@@ -3,18 +3,18 @@ mongoose.set('strictQuery', true);
 
 var mongoURL = 'mongodb+srv://alpha09:alpha119911@cluster0.cd9yba2.mongodb.net/Mern-rooms-adil'
 
-mongoose.connect(process.env.MONGODB_URI || mongoURL ,{useUnifiedTopology : true ,useNewUrlParser : true ,})
+mongoose.connect(process.env.MONGODB_URI || mongoURL, { useUnifiedTopology: true, useNewUrlParser: true, })
 
 
 
 var connection = mongoose.connection
 
-connection.on('error', ()=> {
+connection.on('error', () => {
     console.log('MongDB Connection Failed')
 })
 
-connection.on('connected' , ()=>{
+connection.on('connected', () => {
     console.log('MongoDB Connection Successful')
 })
 
-module.exports  = mongoose
+module.exports = mongoose
